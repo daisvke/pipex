ae37af3d8d8a14375b8c3c66ff0768013adcce5f
dtanigaw <dtanigaw@student.42.fr>
2021-07-05 15:15:15 +0200

    alien

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 02:26:06 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/07/04 10:26:57 by root             ###   ########.fr       */
+/*   Updated: 2021/07/05 14:42:35 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int main(int argc, char *argv[])
 	char    readbuffer[30];
 	int	error;
 
-	char	**cmd;
+	char	**cmd1;
+	char	**cmd2;
 
 	if (pipe(pipe_fds) == -1)
 	{
@@ -36,23 +37,31 @@ int main(int argc, char *argv[])
 	}
 	if (childpid == 0)
 	{
+		cmd2 = ft_split(argv[3], ' ');
 		dup2(pipe_fds[1], 1);
 		close(pipe_fds[0]);
 		close(pipe_fds[1]);
-		gnl_fd = open(argv[1], O_RDONLY);
-		get_next_line(gnl_fd, &gnl_line);
-		write(1, gnl_line, strlen(gnl_line) + 1);
+		error = execvp(cmd2[0], cmd2);
+		if (execvp(cmd2[0], cmd2) == -1)
+		{
+				perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	//	gnl_fd = open(argv[1], O_RDONLY);
+	//	get_next_line(gnl_fd, &gnl_line);
+	//	write(1, gnl_line, strlen(gnl_line) + 1);
 	 }
 	else
 	{
 	//	wait(NULL);
-		cmd = ft_split(argv[2], ' ');
+		cmd1 = ft_split(argv[2], ' ');
 		dup2(pipe_fds[0], 0);
-		close(pipe_fds[1]);
-		close(pipe_fds[0]);
-		read(0, readbuffer, sizeof(readbuffer));
-		error = execvp(cmd[0], cmd) == -1;
-		if (execvp(cmd[0], cmd) == -1)
+//		close(pipe_fds[1]);
+//		close(pipe_fds[0]);
+	//	read(0, readbuffer, sizeof(readbuffer));
+		error = execvp(cmd1[0], cmd1);
+		if (execvp(cmd1[0], cmd1) == -1)
 		{
 		//	if (error == EACCESS)
 		//		printf("[ERROR] Permission is denied for a file\n");
@@ -60,7 +69,7 @@ int main(int argc, char *argv[])
 				perror("execvp");
 			exit(EXIT_FAILURE);
 		}
-		printf("Read text is: %s\n", readbuffer);
+//		printf("Read text is: %s\n", readbuffer);
 	}
 	exit(EXIT_SUCCESS);
 }
