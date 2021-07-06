e0eb1951514e2318f4e869f34e810949c647b921
dtanigaw <dtanigaw@student.42.fr>
2021-07-06 15:16:21 +0200

    Separate in child/parent

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 02:26:06 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/07/06 05:36:54 by root             ###   ########.fr       */
+/*   Updated: 2021/07/06 15:15:57 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,61 +18,65 @@ void	ft_exit_when_error_occurs(char *error_message)
 	exit(EXIT_FAILURE);
 }
 
+void	ft_call_child_to_execute_cmd(int *pipe_fds, char **argv)
+{
+	int		fd;
+	char	**cmd1;
+
+	close(0);
+	close(1);
+	if (dup2(pipe_fds[1], 1) == -1)
+		ft_exit_when_error_occurs("dup2");
+	close(pipe_fds[0]);
+	close(pipe_fds[1]);
+	cmd1 = ft_split(argv[2], ' ');
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		ft_exit_when_error_occurs("open");
+	if (execvp(cmd1[0], cmd1) == -1)
+		ft_exit_when_error_occurs("execvp");
+	close(fd);
+}
+
+void	ft_call_parent_to_execute_cmd(int *pipe_fds, char **argv)
+{
+	int 	file_fd;
+	char	**cmd2;
+
+	close(0);
+	close(1);
+	if (dup2(pipe_fds[0], 0) == -1)
+		ft_exit_when_error_occurs("dup2");
+	close(pipe_fds[0]);
+	close(pipe_fds[1]);
+	cmd2 = ft_split(argv[3], ' ');
+	file_fd = open(argv[4], O_WRONLY);
+//	dup2(1, file_fd);
+//	error = execve(cmd2[0], cmd2, NULL);
+	if (execvp(cmd2[0], cmd2) == -1)
+		ft_exit_when_error_occurs("execvp");
+	close(file_fd);
+	wait(NULL);
+}
+
 int main(int argc, char *argv[])
 {
 	int     pipe_fds[2];
-	int		fd;
-	pid_t   childpid;
-	char    readbuffer[30];
+	pid_t   pid;
 
-	char	**cmd1;
-	char	**cmd2;
-    if(argc < 3)
+    if(argc < 5)
     {
-        printf("Usage: ./Step1 <exclusions file> <folder1> <folder2> <folder3> ...\n");
-        return -1;
+        printf("Usage: ./pipex <input file> <cmd1> <cmd2> <output file>\n");
+        exit(EXIT_FAILURE);
     }
 	if (pipe(pipe_fds) == -1)
 		ft_exit_when_error_occurs("pipe");
-	childpid = fork();
-	if (childpid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	if (childpid == 0)
-	{
-		close(0);
-		close(1);
-		dup2(pipe_fds[1], 1);
-		close(pipe_fds[0]);
-		close(pipe_fds[1]);
-		cmd1 = ft_split(argv[2], ' ');
-		open(argv[1], O_RDONLY);
-		if (fd == -1)
-			ft_exit_when_error_occurs("open");
-		if (execvp(cmd1[0], cmd1) == -1)
-			ft_exit_when_error_occurs("execvp");
-		close(fd);
-		//	if (error == EACCESS)
-		//		printf("[ERROR] Permission is denied for a file\n");
-	}
+	pid = fork();
+	if (pid == -1)
+		ft_exit_when_error_occurs("fork");
+	if (pid == 0)
+		ft_call_child_to_execute_cmd(pipe_fds, argv);
 	else
-	{
-		int file_fd;
-		close(0);
-		dup2(pipe_fds[0], 0);
-		close(pipe_fds[0]);
-		close(pipe_fds[1]);
-		cmd2 = ft_split(argv[3], ' ');
-		file_fd = open(argv[4], O_WRONLY);
-	//	dup2(1, file_fd);
-	//	error = execve(cmd2[0], cmd2, NULL);
-		if (execvp(cmd2[0], cmd2) == -1)
-			ft_exit_when_error_occurs("execvp");
-	//	while (read(
-		close(file_fd);
-		wait(NULL);
-	}
+		ft_call_parent_to_execute_cmd(pipe_fds, argv);
 	exit(EXIT_SUCCESS);
 }
