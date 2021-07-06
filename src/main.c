ec7797a6cff5d608e586803bda7965db9e5459db
dtanigaw <dtanigaw@student.42.fr>
2021-07-06 05:37:32 +0200

    Redirection to output file works

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 02:26:06 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/07/06 05:01:55 by root             ###   ########.fr       */
+/*   Updated: 2021/07/06 05:36:54 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int main(int argc, char *argv[])
 	int		fd;
 	pid_t   childpid;
 	char    readbuffer[30];
-	int		error;
 
 	char	**cmd1;
 	char	**cmd2;
@@ -49,11 +48,10 @@ int main(int argc, char *argv[])
 		close(pipe_fds[0]);
 		close(pipe_fds[1]);
 		cmd1 = ft_split(argv[2], ' ');
-		fd = open(argv[1], O_RDONLY);
+		open(argv[1], O_RDONLY);
 		if (fd == -1)
 			ft_exit_when_error_occurs("open");
-		error = execvp(cmd1[0], cmd1);
-		if (error == -1)
+		if (execvp(cmd1[0], cmd1) == -1)
 			ft_exit_when_error_occurs("execvp");
 		close(fd);
 		//	if (error == EACCESS)
@@ -61,15 +59,19 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		int file_fd;
 		close(0);
 		dup2(pipe_fds[0], 0);
+		close(pipe_fds[0]);
 		close(pipe_fds[1]);
 		cmd2 = ft_split(argv[3], ' ');
-		error = execvp(cmd2[0], cmd2);
-//		error = execve(cmd2[0], cmd2, NULL);
-		if (error == -1)
+		file_fd = open(argv[4], O_WRONLY);
+	//	dup2(1, file_fd);
+	//	error = execve(cmd2[0], cmd2, NULL);
+		if (execvp(cmd2[0], cmd2) == -1)
 			ft_exit_when_error_occurs("execvp");
 	//	while (read(
+		close(file_fd);
 		wait(NULL);
 	}
 	exit(EXIT_SUCCESS);
