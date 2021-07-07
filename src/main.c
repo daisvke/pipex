484c9ea23ebad62861b5fa427d2b40da9f421722
dtanigaw <dtanigaw@student.42.fr>
2021-07-07 17:47:08 +0200

    bofore bb

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 02:26:06 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/07/06 15:15:57 by root             ###   ########.fr       */
+/*   Updated: 2021/07/07 16:46:15 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,28 +38,29 @@ void	ft_call_child_to_execute_cmd(int *pipe_fds, char **argv)
 	close(fd);
 }
 
-void	ft_call_parent_to_execute_cmd(int *pipe_fds, char **argv)
+void	ft_call_parent_to_execute_cmd(int *pipe_fds, char *argv[], char *envp[])
 {
 	int 	file_fd;
-	char	**cmd2;
+	char	*cmd2[3] = { "/bin/cat", "-e", 0};
 
+	wait(NULL);
 	close(0);
 	close(1);
 	if (dup2(pipe_fds[0], 0) == -1)
 		ft_exit_when_error_occurs("dup2");
 	close(pipe_fds[0]);
 	close(pipe_fds[1]);
-	cmd2 = ft_split(argv[3], ' ');
-	file_fd = open(argv[4], O_CREAT | O_WRONLY);
+//	cmd2 = ft_split(argv[3], ' ');
+	file_fd = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	dup2(1, file_fd);
-//	error = execve(cmd2[0], cmd2, NULL);
-	if (execvp(cmd2[0], cmd2) == -1)
-		ft_exit_when_error_occurs("execvp");
+	if (execve("/bin/cat", cmd2, NULL) == -1)
+		ft_exit_when_error_occurs("execve");
+//	if (execvp(cmd2[0], cmd2) == -1)
+//		ft_exit_when_error_occurs("execvp");
 	close(file_fd);
-	wait(NULL);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
 	int     pipe_fds[2];
 	pid_t   pid;
@@ -77,6 +78,6 @@ int main(int argc, char *argv[])
 	if (pid == 0)
 		ft_call_child_to_execute_cmd(pipe_fds, argv);
 	else
-		ft_call_parent_to_execute_cmd(pipe_fds, argv);
+		ft_call_parent_to_execute_cmd(pipe_fds, argv, envp);
 	exit(EXIT_SUCCESS);
 }
